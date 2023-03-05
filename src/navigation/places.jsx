f23be616f13e2f62b02cf491f7301b2b9e9c4e63
Smael Nicolas <nico.smael@gmail.com>
2023-03-05 19:14:19 -0300

    add icon to navigate

--- src/navigation/places.jsx
+++ src/navigation/places.jsx
@@ -1,39 +1,65 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
-import { Platform } from "react-native";
+import { Platform, TouchableOpacity } from "react-native";
 
-import { MapsScreen, NewPlaceScreen, PlaceDetailScreen, PlaceListScreen } from "../screens/index";
+import {
+	MapsScreen,
+	NewPlaceScreen,
+	PlaceDetailScreen,
+	PlaceListScreen,
+} from "../screens/index";
 import colors from "../utils/colors";
 
 const Stack = createNativeStackNavigator();
 
 const PlacesNavigator = () => {
-  return (
-    <Stack.Navigator
-      initialRouteName="Place"
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: Platform.OS === "android" ? colors.primary : colors.secondary,
-        },
-        headerTintColor: colors.black,
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
-      }}>
-      <Stack.Screen name="Places" component={PlaceListScreen} options={{ title: "Direcciones" }} />
-      <Stack.Screen
-        name="PlaceDetail"
-        component={PlaceDetailScreen}
-        options={{ title: "Detalles de la dirección" }}
-      />
-      <Stack.Screen
-        name="NewPlace"
-        component={NewPlaceScreen}
-        options={{ title: "Nueva dirección" }}
-      />
-      <Stack.Screen name="Maps" component={MapsScreen} options={{ title: "Mapa" }} />
-    </Stack.Navigator>
-  );
+	return (
+		<Stack.Navigator
+			initialRouteName="Place"
+			screenOptions={{
+				headerStyle: {
+					backgroundColor: colors.primary,
+				},
+				headerTintColor: colors.black,
+				headerTitleStyle: {
+					fontWeight: "bold",
+				},
+			}}>
+			<Stack.Screen
+				name="Places"
+				component={PlaceListScreen}
+				options={({ navigation }) => ({
+					title: "Direcciones",
+					headerRight: () => (
+						<TouchableOpacity
+							onPress={() => navigation.navigate("NewPlace")}>
+							<Ionicons
+								name="add"
+								size={24}
+								color={colors.black}
+							/>
+						</TouchableOpacity>
+					),
+				})}
+			/>
+			<Stack.Screen
+				name="PlaceDetail"
+				component={PlaceDetailScreen}
+				options={{ title: "Detalles de la dirección" }}
+			/>
+			<Stack.Screen
+				name="NewPlace"
+				component={NewPlaceScreen}
+				options={{ title: "Nueva dirección" }}
+			/>
+			<Stack.Screen
+				name="Maps"
+				component={MapsScreen}
+				options={{ title: "Mapa" }}
+			/>
+		</Stack.Navigator>
+	);
 };
 
 export default PlacesNavigator;
