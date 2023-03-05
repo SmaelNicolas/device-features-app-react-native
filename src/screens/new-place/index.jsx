f5854d4db725520d804608f66e9ac342f2def48c
Smael Nicolas <nico.smael@gmail.com>
2023-03-05 19:31:30 -0300

    addPlace view

--- src/screens/new-place/index.jsx
+++ src/screens/new-place/index.jsx
@@ -1,13 +1,25 @@
-import { View, Text } from "react-native";
+import { Button, ScrollView, Text, TextInput, View } from "react-native";
+import colors from "../../utils/colors";
 
 import { styles } from "./styles";
 
 const NewPlace = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Text>New Place</Text>
-    </View>
-  );
+	return (
+		<ScrollView style={styles.container}>
+			<View style={styles.content}>
+				<Text style={styles.title}>Lugar</Text>
+				<TextInput
+					style={styles.input}
+					placeholder="Escribe el nombre del lugar"
+				/>
+				<Button
+					color={colors.primary}
+					title="Guardar"
+					onPress={() => {}}
+				/>
+			</View>
+		</ScrollView>
+	);
 };
 
 export default NewPlace;
