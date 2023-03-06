9be39080a76f874f284fb166e113ea90997ffa49
Smael Nicolas <nico.smael@gmail.com>
2023-03-06 14:03:35 -0300

    flat list

--- src/screens/place-list/index.jsx
+++ src/screens/place-list/index.jsx
@@ -1,15 +1,37 @@
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import { useSelector } from "react-redux";
+import { PlaceItem } from "../../components/place-item";
 
 import { styles } from "./styles";
 
 const PlaceList = ({ navigation }) => {
 	const places = useSelector((state) => state.place.places);
 
-	return (
-		<View style={styles.container}>
-			<Text>Place List</Text>
+	const renderItem = ({ item }) => (
+		<PlaceItem
+			{...item}
+			onSelect={() =>
+				navigation.navigate("PlaceDetail", { placeId: item.id })
+			}
+		/>
+	);
+
+	const ListEmptyComponent = () => (
+		<View style={styles.emptyContainer}>
+			<Text style={styles.emptyText}>Crea un nuevo lugar ↗</Text>
 		</View>
+	);
+
+	const keyExtractor = (item) => item.id;
+
+	return (
+		<FlatList
+			style={styles.container}
+			data={places}
+			keyExtractor={keyExtractor}
+			renderItem={renderItem}
+			ListEmptyComponent={ListEmptyComponent}
+		/>
 	);
 };
 
