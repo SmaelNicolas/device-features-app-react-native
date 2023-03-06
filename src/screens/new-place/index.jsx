42c2284f7f793624265c28f46f98ce57cb664693
Smael Nicolas <nico.smael@gmail.com>
2023-03-06 00:09:31 -0300

    add component image, input

--- src/screens/new-place/index.jsx
+++ src/screens/new-place/index.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import { Button, ScrollView, Text, TextInput, View } from "react-native";
+import { useDispatch } from "react-redux";
+import { ImageSelector } from "../../components/image-selector";
+import { addPlace } from "../../store/place.slice";
 import colors from "../../utils/colors";
 
 import { styles } from "./styles";
 
 const NewPlace = ({ navigation }) => {
+	const [title, setTitle] = useState("");
+
+	const dispatch = useDispatch();
+
+	const onHandleText = (txt) => {
+		setTitle(txt);
+	};
+
+	const onHandleSubmit = () => {
+		dispatch(addPlace({ title }));
+		navigation.goBack();
+	};
+
 	return (
 		<ScrollView style={styles.container}>
 			<View style={styles.content}>
@@ -11,11 +28,15 @@ const NewPlace = ({ navigation }) => {
 				<TextInput
 					style={styles.input}
 					placeholder="Escribe el nombre del lugar"
+					onChangeText={onHandleText}
+					value={title}
 				/>
+				<ImageSelector />
 				<Button
 					color={colors.primary}
 					title="Guardar"
-					onPress={() => {}}
+					disabled={title.length === 0}
+					onPress={onHandleSubmit}
 				/>
 			</View>
 		</ScrollView>
