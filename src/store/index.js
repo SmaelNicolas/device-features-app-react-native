41d32c68f6fe8cd026c9f72b8c475ef26a6fc5c1
Smael Nicolas <nico.smael@gmail.com>
2023-03-06 00:09:51 -0300

    configure reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
 import placeReducer from "./place.slice";
 
 export const store = configureStore({
-  reducer: {
-    place: placeReducer,
-  },
+	reducer: {
+		place: placeReducer,
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({ serializableCheck: false }),
 });
