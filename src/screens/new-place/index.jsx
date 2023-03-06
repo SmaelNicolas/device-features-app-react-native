7f62eb5b5988f6c5923901e06431623e6e60f2a3
Smael Nicolas <nico.smael@gmail.com>
2023-03-06 14:03:15 -0300

    function names

--- src/screens/new-place/index.jsx
+++ src/screens/new-place/index.jsx
@@ -1,46 +1,49 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, ScrollView, Text, TextInput, View } from "react-native";
 import { useDispatch } from "react-redux";
-import { ImageSelector } from "../../components/image-selector";
+
+import { ImageSelector } from "../../components";
 import { addPlace } from "../../store/place.slice";
 import colors from "../../utils/colors";
-
 import { styles } from "./styles";
 
 const NewPlace = ({ navigation }) => {
-	const [title, setTitle] = useState("");
-	const [image, setImage] = useState(null);
-
 	const dispatch = useDispatch();
+	const [title, setTitle] = useState("");
+	const [image, setImage] = useState("");
 
-	const onHandleText = (txt) => {
-		setTitle(txt);
-	};
-
-	const onHandleImageSelect = (imageUrl) => {
-		setImage(imageUrl);
+	const onHandleChange = (text) => {
+		setTitle(text);
 	};
 
 	const onHandleSubmit = () => {
-		dispatch(addPlace({ title, image }));
-		navigation.goBack();
+		dispatch(addPlace(title, image));
+		navigation.navigate("Places");
 	};
+
+	const onImage = (urlImage) => {
+		console.log("ONIMAGE", urlImage);
+		setImage(urlImage);
+	};
+
+	useEffect(() => {
+		console.log("USEEFFECT", image);
+	}, [image]);
 
 	return (
 		<ScrollView style={styles.container}>
 			<View style={styles.content}>
-				<Text style={styles.title}>Lugar</Text>
+				<Text style={styles.title}>Titulo</Text>
 				<TextInput
 					style={styles.input}
-					placeholder="Escribe el nombre del lugar"
-					onChangeText={onHandleText}
+					placeholder="Nuevo ubicacion"
+					onChangeText={onHandleChange}
 					value={title}
 				/>
-				<ImageSelector onImage={onHandleImageSelect} />
+				<ImageSelector onImage={onImage} />
 				<Button
+					title="Grabar dirección"
 					color={colors.primary}
-					title="Guardar"
-					disabled={title.length === 0}
 					onPress={onHandleSubmit}
 				/>
 			</View>
